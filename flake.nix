dd5496d40b49885c36bf8fd3c4a7a1d60d9a2457
Markus Hauck <markus1189@gmail.com>
2022-12-10 21:24:01 +0100

    Reduce json

--- flake.nix
+++ flake.nix
@@ -12,9 +12,10 @@
 
           cat channels.txt |
             parallel --line-buffer -n1 -j 10 -I{} yt-dlp --dump-json "{}" |
+            jq -c 'delpaths([["formats"],["thumbnails"],["automatic_captions"],["subtitles"]])' |
             tee content.json |
             pv -betlapf -i 30 > /dev/null
         '';
-      in mkShell { buildInputs = [ yt-dlp parallel runScript git-lfs pv ]; };
+      in mkShell { buildInputs = [ yt-dlp parallel runScript git-lfs pv jq ]; };
   };
 }
