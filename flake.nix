2c5acf77600f9053868cb2ab7cfe508e762ca1de
Markus Hauck <markus1189@gmail.com>
2022-12-10 19:22:23 +0100

    Force output with pv

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
           cat channels.txt |
             parallel --line-buffer -n1 -j 10 -I{} yt-dlp --dump-json "{}" |
             tee content.json |
-            pv -betlap l > /dev/null
+            pv -betlapf > /dev/null
         '';
       in mkShell { buildInputs = [ yt-dlp parallel runScript git-lfs pv ]; };
   };
